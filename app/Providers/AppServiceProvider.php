257f6b575fcf06386580fbcbcce48d718f71d226
Kévin <voisine.kevin2@gmail.com>
2025-08-15 19:08:54 +0200

    Resolve "Vercel"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::useScriptTagAttributes(['defer' => false]);
+        Vite::useScriptTagAttributes(['defer' => true]);
+
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
